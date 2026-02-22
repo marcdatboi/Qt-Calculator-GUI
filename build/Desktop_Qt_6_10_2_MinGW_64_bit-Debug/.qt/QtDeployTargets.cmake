e373569457194b913361a6481064edb2b038c54d
marcdatboi <marcmauricebacongcobell@gmail.com>
2026-02-22 14:22:52 -0800

    Initial Commit

---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Qt-Calculator-GUI_FILE C:/DevFolder/C++/projects/Qt-Calculator-GUI/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Qt-Calculator-GUI.exe)
+set(__QT_DEPLOY_TARGET_Qt-Calculator-GUI_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Qt-Calculator-GUI_RUNTIME_DLLS C:/DevFolder/tools/APIs/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/DevFolder/tools/APIs/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/DevFolder/tools/APIs/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
